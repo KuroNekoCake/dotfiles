1de03e5528a902d06ee771a3b4f282515e6768a0
KuroNekoCake <SorCyyy@gmail.com>
2023-11-06 18:53:04 +0900

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(.zshrc .tmux.conf)
+dotfiles=(.zshrc .tmux.conf .test)
 
 # .zshrc と .tmux.conf という設定ファイルのシンボリックリンクを
 # ホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
-        ln -svf $file ~/
+        ln -svf $HOME/dotfiles/$file ~/
+	#ln -svf $file ~/
 done
