b6bbe372c8ea378597a2a1e9bc7eddbfbd054164
KuroNekoCake <SorCyyy@gmail.com>
2023-11-06 20:44:46 +0900

    add

--- .zshrc
+++ .zshrc
@@ -76,3 +76,7 @@ SCRIPT_DIR=$HOME/dotfiles
 source $SCRIPT_DIR/zsh/plugins.zsh
 source $SCRIPT_DIR/zsh/config.zsh
 source $SCRIPT_DIR/zsh/p10k.zsh
+source $SCRIPT_DIR/zsh/peco.zsh
+source $SCRIPT_DIR/zsh/cdr.zsh
+
+PATH=$PATH:$HOME/bin:/usr/local/go/bin
---
+++ zsh/peco.zsh
@@ -0,0 +1,18 @@
+function peco-history-selection() {
+    BUFFER=`history -n 1 | tac  | awk '!a[$0]++' | peco`
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+
+zle -N peco-history-selection
+bindkey '^R' peco-history-selection
+
+function peco-cdr () {
+    local selected_dir="$(cdr -l | sed 's/^[0-9]\+ \+//' | peco --prompt="cdr >" --query "$LBUFFER")"
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+}
+zle -N peco-cdr
+bindkey '^E' peco-cdr
