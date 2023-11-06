9ac921c0e31771915826db19640cc78f4b26b512
KuroNekoCake <SorCyyy@gmail.com>
2023-11-06 19:23:18 +0900

    add p10k

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(.zshrc .tmux.conf .test)
+dotfiles=(.zshrc)
 
 # .zshrc と .tmux.conf という設定ファイルのシンボリックリンクを
 # ホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
         ln -svf $HOME/dotfiles/$file ~/
-	#ln -svf $file ~/
+done
+
+#!/bin/bash
+# 配置したい設定ファイル
+zsh=(.tmux.conf .p10k.zsh)
+
+# .zshrc と .tmux.conf という設定ファイルのシンボリックリンクを
+# ホームディレクトリ直下に作成する
+for zshFile in "${zsh[@]}"; do
+        ln -svf $HOME/dotfiles/zsh/$zshFile ~/
 done
